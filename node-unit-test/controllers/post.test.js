8a8b6be641a3b91e6cab06c1c9fe8bb344d2139c
Aldwin <91401877+goopiktu@users.noreply.github.com>
2024-02-04 21:35:31 +0800

    update: node-unit-test

--- node-unit-test/controllers/post.test.js
+++ node-unit-test/controllers/post.test.js
@@ -1,10 +1,49 @@
 import { addPost } from "./postController";
 import { jest } from "@jest/globals";
-import { sinon } from "sinon";
 
 describe('Post controller', () => {
     
 
 
-    describe('')
+
+  
+    describe('Add Post', () => {
+        it('should add a new post', () => {
+            const req = {
+                body: {
+                    title: 'Test Post',
+                    content: 'Test Content',
+                    author: 'Test Author'
+                }
+            };
+
+            const res = {
+                json: jest.fn(),
+                status: jest.fn().mockReturnThis(),
+                end: jest.fn()
+            };
+
+            addPost(req, res);
+
+            // Assuming your implementation saves the post and returns some result
+            expect(res.json).toHaveBeenCalledWith(expect.objectContaining({
+                title: 'Test Post',
+                content: 'Test Content',
+                author: 'Test Author'
+            }));
+        });
+    });
+
+    // Add tests for other functions like 'Get User Post' and 'Get Post' here
+    
+
+
+
+    describe('Get User Post', () => {
+
+    });
+
+    describe('Get Post', () => {
+         
+    })
 })
